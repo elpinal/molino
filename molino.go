346cd87926aba3be15f9fa3956d69a77faa696a2
elpinal <elpinal@users.noreply.github.com>
2014-12-07 09:19:31 +0900

    Body is one line or one file

--- molino.go
+++ molino.go
@@ -3,6 +3,7 @@ package main
 import (
   "./vm"
   go_core "./builtins/go"
+  "flag"
   "fmt"
   "io/ioutil"
   "log"
@@ -10,36 +11,52 @@ import (
   "reflect"
 )
 
+var fs = flag.NewFlagSet(os.Args[0], 1)
+var e  = fs.String("e", "", "One line of program")
+
 func main() {
+  fs.Parse(os.Args[1:])
   env := vm.NewEnv()
-  go_core.Import(env)
-  for _, arg := range os.Args[1:] {
-    scanner := new(vm.Scanner)
-    body, err := ioutil.ReadFile(arg)
+  var body []byte
+  var source string
+  if *e != "" {
+    body  = []byte(*e)
+    source = "argument"
+  } else {
+    var err error
+    arg := os.Args[1]
+    source = fs.Arg(0)
+    body, err = ioutil.ReadFile(arg)
     if err != nil {
       log.Fatal(err)
     }
-    scanner.Init(string(body))
-    for _, statement := range vm.Parse(scanner) {
-      s, err := vm.Run(statement, env)
-      if err != nil {
-        log.Fatal(err)
-      }
-      //fmt.Println(s)
-      var keyword vm.Keyword
-      o, ok := s.Interface().(fmt.Stringer)
-      switch {
-      case s.Kind() != reflect.String && ok:
-        fmt.Println(o)
-      case s.Kind() == reflect.Slice:
-        fmt.Println(s.Interface())
-      case s.Kind() == reflect.Map:
-        fmt.Println(s.Interface())
-      case s.Type() == reflect.TypeOf(keyword):
-        fmt.Printf(":%v\n", s.Interface())
-      default:
-        fmt.Printf("%#v\n", s.Interface())
-      }
+  }
+  os.Args = fs.Args()
+
+  go_core.Import(env)
+
+  scanner := new(vm.Scanner)
+  scanner.Init(string(body))
+  for _, statement := range vm.Parse(scanner) {
+    s, err := vm.Run(statement, env)
+    if err != nil {
+      fmt.Printf("%s: ", source)
+      log.Fatal(err)
+    }
+    //fmt.Println(s)
+    var keyword vm.Keyword
+    o, ok := s.Interface().(fmt.Stringer)
+    switch {
+    case s.Kind() != reflect.String && ok:
+      fmt.Println(o)
+    case s.Kind() == reflect.Slice:
+      fmt.Println(s.Interface())
+    case s.Kind() == reflect.Map:
+      fmt.Println(s.Interface())
+    case s.Type() == reflect.TypeOf(keyword):
+      fmt.Printf(":%v\n", s.Interface())
+    default:
+      fmt.Printf("%#v\n", s.Interface())
     }
   }
 }
