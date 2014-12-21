376fc5e9e5981887c51bcf3c594883702fbb4858
elpinal <elpinal@users.noreply.github.com>
2014-12-22 00:21:34 +0900

    Support some escape character

--- src/go/molino/lang/Fn.go
+++ src/go/molino/lang/Fn.go
@@ -14,7 +14,28 @@ func (f StringReader) invoke(r *Reader,doublequote rune) interface{} {
 			panic("EOF while reading string")
 		}
 		if ch == '\\' {
-			//
+			ch := r.read()
+			if ch == -1 {
+				panic("EOF while reading string")
+			}
+			switch ch {
+			case 't':
+				ch = '\t'
+			case 'r':
+				ch = '\r'
+			case 'n':
+				ch = '\n'
+			case '\\':
+			case '"':
+			case 'b':
+				ch = '\b'
+			case 'f':
+				ch = '\f'
+			case 'u':
+				ch = r.read()
+				if !( ( '0' <= ch && ch <= '9' ) || ( 'a' <= ch && ch <= 'f' ) ) {
+					panic("Invalid unicode escape \\u" + string(ch))
+				}
 		}
 		ret = append(ret, ch)
 	}
