26f409bb84f7ce36d07241c504e7a3d9782c0a54
elpinal <elpinal@users.noreply.github.com>
2014-12-21 21:55:30 +0900

    Gofmt

--- molino.go
+++ molino.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-  "./src/go/molino/lang"
-  "flag"
-  "io/ioutil"
-  "log"
-  "os"
+	"./src/go/molino/lang"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var fs = flag.NewFlagSet(os.Args[0], 1)
-var e  = fs.String("e", "", "One line of program")
+var e = fs.String("e", "", "One line of program")
 
 func main() {
-  fs.Parse(os.Args[1:])
-  var body []byte
-  var source string
-  if *e != "" {
-    body  = []byte(*e)
-    source = "argument"
-  } else {
-    var err error
-    arg := os.Args[1]
-    source = fs.Arg(0)
-    body, err = ioutil.ReadFile(arg)
-    if err != nil {
-      log.Fatal(err)
-    }
-  }
-  os.Args = fs.Args()
+	fs.Parse(os.Args[1:])
+	var body []byte
+	var source string
+	if *e != "" {
+		body = []byte(*e)
+		source = "argument"
+	} else {
+		var err error
+		arg := os.Args[1]
+		source = fs.Arg(0)
+		body, err = ioutil.ReadFile(arg)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	os.Args = fs.Args()
 
-  lang.Runtime()
+	lang.Runtime()
 }
