b06ca87201fe1974804e773c2feda770c1e8ce36
elpinal <elpinal@users.noreply.github.com>
2014-12-23 21:12:49 +0900

    Add lang_test.go

---
+++ src/go/molino/lang/lang_test.go
@@ -0,0 +1,16 @@
+package lang
+
+import (
+	"testing"
+)
+
+func TestVarIntern(t *testing.T) {
+	var v Var
+	var ns Namespace = FindOrCreate(intern("testnamespace"))
+	var sym Symbol = intern("testsymbol")
+
+	var ret Var = v.intern(ns, sym, 4, true)	
+	if ret.root != 4 {
+		t.Errorf("Expect: %s, but %s", ret.root, 4)
+	}
+}
