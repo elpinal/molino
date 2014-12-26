098da0e6349dce59730e2a52bb9cf7ff1f2a0e8f
elpinal <elpinal@users.noreply.github.com>
2014-12-26 16:53:45 +0900

    Add TransientVector

--- src/go/molino/lang/PersistentVector.go
+++ src/go/molino/lang/PersistentVector.go
@@ -3,6 +3,7 @@ package lang
 type PersistentVector struct {
 	cnt   int
 	shift int
+	root  PersistentVector_Node
 }
 
 type PersistentVector_Node struct {
@@ -10,9 +11,15 @@ type PersistentVector_Node struct {
 	array [32]interface{}
 }
 
+type TransientVector struct {
+	cnt   int
+	shift int
+	root  PersistentVector_Node
+}
+
 var PersistentVector_EMPTY_NODE = PersistentVector_Node{array: [32]interface{}{}}
 
-var PersistentVector_EMPTY = PersistentVector{cnt: 0, shift: 5}
+var PersistentVector_EMPTY = PersistentVector{cnt: 0, shift: 5, root: PersistentVector_EMPTY_NODE}
 
 func (v PersistentVector) create(items ISeq) PersistentVector {
 	var ret = PersistentVector_EMPTY
