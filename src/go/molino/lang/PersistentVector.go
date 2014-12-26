c62e1455520e0049183fd33c5827368d0fe746d3
elpinal <elpinal@users.noreply.github.com>
2014-12-26 13:34:03 +0900

    Add struct, PersistentVector_Node

--- src/go/molino/lang/PersistentVector.go
+++ src/go/molino/lang/PersistentVector.go
@@ -4,3 +4,22 @@ type PersistentVector struct {
 	cnt   int
 	shift int
 }
+
+type PersistentVector_Node struct {
+	//edit
+	array [32]interface{}
+}
+
+var PersistentVector_EMPTY_NODE = PersistentVector_Node{array: [32]interface{}{}}
+
+var PersistentVector_EMPTY = PersistentVector{cnt: 0, shift: 5}
+
+func (v PersistentVector) create(items ISeq) PersistentVector {
+	var ret = PersistentVector_EMPTY
+	_ = ret
+	return PersistentVector{}
+}
+
+func (v PersistentVector) length() int {
+	return v.cnt
+}
