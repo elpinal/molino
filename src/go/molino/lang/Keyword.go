8a11069aa9184e725ffeaa6a04031f0eb0db1c9e
elpinal <elpinal@users.noreply.github.com>
2014-12-28 17:47:53 +0900

    Add Keyword._str

--- src/go/molino/lang/Keyword.go
+++ src/go/molino/lang/Keyword.go
@@ -1,11 +1,19 @@
 package lang
 
 type Keyword struct {
-	sym Symbol
+	sym  Symbol
+	_str string
 }
 
 func (k Keyword) intern(sym Symbol) Keyword {
 	var kw Keyword = Keyword{sym: sym}
 	//
 	return kw
+}
+
+func (k Keyword) String() string {
+	if k._str == "" {
+		k._str = ":" + k.sym.name //
+	}
+	return k._str
 }
