9aef8d881b36aa7ac2649d114716014d8822aecc
elpinal <elpinal@users.noreply.github.com>
2015-02-15 14:57:40 +0900

    Add Cons

---
+++ src/go/molino/lang/Cons.go
@@ -0,0 +1,26 @@
+package lang
+
+type Cons struct {
+	ASeq
+	_first interface{}
+	_more  ISeq
+}
+
+func (c Cons) first() interface{} {
+	return c._first
+}
+
+func (c Cons) next() ISeq {
+	return c.more().seq()
+}
+
+func (c Cons) more() ISeq {
+	if c._more == nil {
+		return EmptyList{}
+	}
+	return c._more
+}
+
+func (c Cons) count() int {
+	return 1 + c._more.count()
+}
