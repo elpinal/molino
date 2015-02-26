bf3466867a6149d516cdae35c740253c65c7d05e
elpinal <elpinal@users.noreply.github.com>
2015-02-26 20:44:35 +0900

    Add Util

---
+++ src/go/molino/lang/Util.go
@@ -0,0 +1,17 @@
+package lang
+
+type util struct {}
+
+var Util util = util{}
+
+func (_ util) hash(o interface{}) int {
+	if o == nil || o == "" {
+		return 0
+	}
+	panic("Cannot create hash")
+}
+
+func (_ util) hashCombine(s, hash int) int {
+	s ^= hash + 0x9e3779 + (seed << 6) + (seed >> 2)
+	return s
+}
